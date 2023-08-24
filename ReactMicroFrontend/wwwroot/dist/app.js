f06627b94d4de45d8dc15372809b3e9b3237ffa3
Will Trombly <76636215+wtrombly@users.noreply.github.com>
2023-08-24 07:37:03 -0500

    Initial commit

---
+++ ReactMicroFrontend/wwwroot/dist/app.js
@@ -0,0 +1,19 @@
+var $ly1wG$reactjsxruntime = require("react/jsx-runtime");
+require("react");
+var $ly1wG$reactdom = require("react-dom");
+
+function $parcel$interopDefault(a) {
+  return a && a.__esModule ? a.default : a;
+}
+
+
+
+window.renderApp = ()=>{
+    const domElement = document.getElementById("react-app") || document.querySelector("#react-app");
+    if (domElement) ($parcel$interopDefault($ly1wG$reactdom)).render(/*#__PURE__*/ $ly1wG$reactjsxruntime.jsx("h1", {
+        children: "Hello, React🌐!"
+    }), domElement);
+};
+
+
+//# sourceMappingURL=app.js.map
